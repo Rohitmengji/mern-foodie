25f26c4907e41ce48f43afff9adab1162c3b5a3c
rohitmengji <rohitmengjih@gmail.com>
2024-02-25 18:14:09 +0530

    allowed origin

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,33 @@
+// const express = require("express");
+// const app = express();
+// const port = 5000;
+
+// global.foodData = require("./db")(function call(err, data, CatData) {
+//   if (err) console.log(err);
+//   global.foodData = data;
+//   global.foodCategory = CatData;
+// });
+
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000",);
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
+// app.use(express.json());
+
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+
+// app.use("/api/auth", require("./Routes/Auth"));
+
+// app.listen(port, () => {
+//   console.log(`app listening on http://localhost:${port}`);
+// });
+
 const express = require("express");
 const app = express();
 const port = 5000;
@@ -8,14 +38,23 @@ global.foodData = require("./db")(function call(err, data, CatData) {
   global.foodCategory = CatData;
 });
 
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://main.d24ftgozfg99e9.amplifyapp.com",
+];
+
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
   next();
 });
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
