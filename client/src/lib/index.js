6d29eb27c71d8e8d2c7575b967a3f52946c391f0
rohitmengji <rohitmengjih@gmail.com>
2024-02-25 17:59:32 +0530

    lib

--- client/src/lib/index.js
+++ client/src/lib/index.js
@@ -1,8 +1,4 @@
 const dev = "http://localhost:5000";
-const prod = "http://ec2-54-152-57-39.compute-1.amazonaws.com:5000";
 
-export const baseURL =
-  window.location.hostname.split(":")[0] === "localhost" ||
-  window.location.hostname.includes("192")
-    ? dev
-    : prod;
+
+export const baseURL = dev
